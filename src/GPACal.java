e297621c23ab85003d4f22f3cdc086f61bfb99d0
MortiestMorty <d.arroyo14@student.keiseruniversity.edu>
2025-03-02 21:50:51 -0500

    fix

--- src/GPACal.java
+++ src/GPACal.java
@@ -1,4 +1,60 @@
-package PACKAGE_NAME;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
-public class GPACal {
-}
+public class GPACal
+{
+    public static void main (String[]args)
+    {
+        Map<String, Double> gradeMap = new HashMap<>();
+
+        gradeMap.put("A+", 4.33);
+        gradeMap.put("A", 4.00);
+        gradeMap.put("A-",3.67);
+        gradeMap.put("B+", 3.33);
+        gradeMap.put("B", 3.00);
+        gradeMap.put("B-",3.67);
+        gradeMap.put("C+", 2.33);
+        gradeMap.put("C", 2.00);
+        gradeMap.put("C-",1.67);
+        gradeMap.put("D", 1.00);
+        gradeMap.put("F", .00);
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter the Letter grade (capital letter): ");
+        String[] grades = in.nextLine().split(" //s+");
+
+        double total = 0;
+        int counter = 0;
+
+        for (String grade : grades)
+        {
+            if (gradeMap.containsKey(grade))
+            {
+                total += gradeMap.get(grade);
+                counter++;
+
+            }// end if
+                else
+                {
+                    System.out.println("Invalid grade: " + grade);
+                    break;
+                }// end else
+
+        }// end for
+
+        if (counter > 0)
+        {
+            double gpa = (total / counter);
+            System.out.println("Your GPA is " + gpa);
+        }// end if
+        else
+        {
+            System.out.println("No grades were found");
+        }// end else statement
+
+
+
+
+    }// end main
+}// end class
